c00d7e106a36aa094ea19f1a36c749a835784ddb
PR31SL3R <phf360@alumni.ku.dk>
2014-04-30 14:34:14 +0200

    slider added

---
+++ slider/carls.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="themes/1/js-image-slider.css" rel="stylesheet" type="text/css" />
+    <script src="themes/1/js-image-slider.js" type="text/javascript"></script>
+    <link href="generic.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div id="sliderFrame">
+        <div id="slider">
+            <a href="http://www.menucool.com/javascript-image-slider" target="_blank">
+                <img src="images/image-slider-1.jpg" alt="Carls Barber Shop" />
+            </a>
+            <img src="images/image-slider-2.jpg" alt="Est. 1941" />
+            <img src="images/image-slider-3.jpg" alt="Carls Barber Shop" />
+            <img src="images/image-slider-4.jpg" alt="#htmlcaption" />
+            <img src="images/image-slider-5.jpg" alt=""/>
+        </div>
+        <div id="htmlcaption" style="display: none;">
+            Book en tid <a href="http://jegvilbestilletid.dk/haircon.osterbrogade">nu</a>
+        </div>
+    </div>
+</body>
+</html>
